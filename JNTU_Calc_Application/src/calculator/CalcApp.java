2958394dc411c593a01038f3b73532713d0ce38d
Deeparna4 <gdeeparna@gmail.com>
2025-11-07 22:06:27 +0530

    Modified main java class

--- JNTU_Calc_Application/src/calculator/CalcApp.java
+++ JNTU_Calc_Application/src/calculator/CalcApp.java
@@ -9,7 +9,7 @@ public class CalcApp {
         int a = sc.nextInt();
         System.out.println("Enter second number: ");
         int b = sc.nextInt();
-        System.out.println("Sum = " + (a + b));
+        System.out.println("Total Sum = " + (a + b));
         sc.close();
     }
 }
